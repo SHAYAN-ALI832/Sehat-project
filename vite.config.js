bfe975f0c5c6053919443ecf4556ad44075cde43
SHAYAN-ALI832 <shayan.ali.mansehra@gmail.com>
2025-01-21 16:43:55 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SUMPW ', // Replace 'SUMPW' with your repository name
+  base: '/Sehat-project', // Replace 'SUMPW' with your repository name
 });
